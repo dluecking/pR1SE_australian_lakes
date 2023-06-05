f38c529aa3137a629868e7a00694dbeaa8bc477b
dluecking <dom.luecking@gmail.com>
2023-06-05 10:16:32 +0200

    after new initial proteins

--- generate_protein_clusters/scripts/00_download_initial_proteins.R
+++ generate_protein_clusters/scripts/00_download_initial_proteins.R
@@ -1,22 +1,28 @@
-author: dlueckin
+# author: dlueckin
 # date: Tue Apr 18 10:54:15 2023
 
 # libraries ---------------------------------------------------------------
 library(data.table)
 library(dplyr)
 library(rentrez)
+library(tidyr)
 
+
+# working directory -------------------------------------------------------
+this_dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(this_dir)
 
 # load clean df -----------------------------------------------------------
 # local
-INPUT_PATH <- "pR1SE core set - clean input.csv"
+INPUT_PATH <- "../pR1SE core set - clean input.csv"
 
 # read input df
-input_df <- fread(INPUT_PATH)
+input_df <- fread(INPUT_PATH) %>% 
+    select(1:6)
 
 # convert to long
 input_df <- input_df %>% 
-    pivot_longer(-c(pR1SE, `predicted function`), names_to = "species", values_to = "accession") %>% 
+    pivot_longer(-c(ORF, `predicted function`), names_to = "species", values_to = "accession") %>% 
     mutate(species = gsub(" ", "_", species))
 names(input_df) <- c("ORF", "predicted_function", "species", "accession")
 
@@ -29,7 +35,7 @@ for(current_ORF in input_df$ORF){
         select(accession) %>% 
         unlist()
     
-    FILENAME <- paste0("initial_proteins/", current_ORF, "_initial_proteins.faa")
+    FILENAME <- paste0("../01_input_proteins/", current_ORF, "_initial_proteins.faa")
     
     sequences <- entrez_fetch(db="protein", id=list_of_accessions, rettype = "fasta")
     write(x = sequences, file = FILENAME)
